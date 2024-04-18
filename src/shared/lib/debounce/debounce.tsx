cc5523623221566707980765a688efe2b3c02011
shinoki <endernasha85@gmail.com>
2024-04-18 20:39:51 +0300

    shared utils

---
+++ src/shared/lib/debounce/debounce.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+
+const useDebounce = (value: string, delay: number) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+  
+    useEffect(() => {
+        const handler = setTimeout(() => setDebouncedValue(value), delay);
+
+		return () => clearTimeout(handler);
+	}, [value]);
+  
+    return debouncedValue;
+}
+
+export default useDebounce;

858d1e83b4dd03a959f414b87677f835a529b8b1
shinoki <endernasha85@gmail.com>
2024-04-18 23:32:23 +0300

    image loading mock

--- src/shared/assets/svg/icons/index.ts
+++ src/shared/assets/svg/icons/index.ts
@@ -1,9 +1,11 @@
 import BackIcon from './back.svg?react';
 import MenuIcon from './menu.svg?react';
 import SpinnerIcon from './spinner.svg?react';
+import ImageLoading from './image_loading.svg?react';
 
 export {
     BackIcon, 
     MenuIcon,
     SpinnerIcon,
+    ImageLoading,
 }
--- src/shared/ui/image/image.tsx
+++ src/shared/ui/image/image.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './image.module.scss';
+import { ImageLoading } from 'shared/assets/svg/icons';
 
 interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
     className?: string;
@@ -8,16 +9,23 @@ interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
 
 export const Image = ({ className = '', src = '', alt = '' }: ImageProps) => {
     const [imageError, setImageError] = useState(false);
+    const [imageLoaded, setImageLoaded] = useState(false);
+
     const seed = Math.floor(Math.random() * 1000);
 
-    const handleImageError = () => setImageError(true); 
+    const handleImageError = () => { setImageError(true); setImageLoaded(true); }
+    const handleImageLoaded = () => setImageLoaded(true);
 
     return (
         <div className={classNames(cls.image, {}, [className])}>
+            {!imageLoaded ? 
+                <ImageLoading width="100%" height="100%" />
+                : null
+            }
             {imageError ? (
                 <img src={`https://picsum.photos/200/300?random=${seed}`} alt="Mock image" />
             ) : (
-                <img src={src} alt={alt} onError={handleImageError} />
+                <img src={src} alt={alt} onError={handleImageError} onLoad={handleImageLoaded} />
             )}
         </div>
     );

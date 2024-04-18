62d7aa9f0da7d6b158b064214e1d791a4b175bfe
shinoki <endernasha85@gmail.com>
2024-04-18 20:30:29 +0300

    app configurations for types & styles

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = async () => {
     return {
       verbose: true,
       testMatch: ['**/*.test.ts?(x)'],
+      preset: 'ts-jest',
+      testEnvironment: 'node',
     };
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr' 
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    tsconfigPaths(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+  resolve: {
+      alias: {
+          "@": path.resolve(__dirname, "src")
+      },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/styles/index.scss";
+        `,
+      },
+    },
+  },
 })
